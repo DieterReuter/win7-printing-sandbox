ecc7d4dfd08bc50d9a6369a5616b641d796558db
Dieter Reuter <dieter.reuter@me.com>
2014-02-16 13:57:55 +0100

    Always create a packer build log file

--- build-basebox.sh
+++ build-basebox.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 basebox=./template/windows
 flavour=virtualbox
+template=windows7x64sp1
 
 echo "Building basebox: ${basebox} for ${flavour}"
 
@@ -10,6 +11,13 @@ if [ "${PACKER_CACHE_DIR}x" == "x" ]; then
 fi
 echo "...using PACKER_CACHE_DIR = $PACKER_CACHE_DIR"
 
+# let's get a packer build log
+mkdir -p ./log
+export PACKER_LOG=1
+export PACKER_LOG_PATH=`pwd`/log/${template}.log
+echo "...using PACKER_LOG = $PACKER_LOG"
+echo "...using PACKER_LOG_PATH = $PACKER_LOG_PATH"
+
 # now run the Packer build
 mkdir -p ${flavour}
-(cd ${basebox}; packer build -only=${flavour}-iso windows7x64sp1.json)
+(cd ${basebox}; packer build -only=${flavour}-iso ${template}.json)
