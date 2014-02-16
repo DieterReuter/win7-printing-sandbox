a253dff6660d8d9d0020a86ffe902e344ab19370
Dieter Reuter <dieter.reuter@me.com>
2014-02-16 12:50:46 +0100

    Use a packer_cache dir in the top level dir or use a global PACKER_CACHE_DIR variable

--- build-basebox.sh
+++ build-basebox.sh
@@ -3,5 +3,13 @@ basebox=./template/dieter-win7x64sp1
 flavour=virtualbox
 
 echo "Building basebox: ${basebox} for ${flavour}"
+
+# use of local or global PACKER_CACHE_DIR to store the downloaded .ISO files
+if [ "${PACKER_CACHE_DIR}x" == "x" ]; then
+  export PACKER_CACHE_DIR="`pwd`/packer_cache"
+fi
+echo "...using PACKER_CACHE_DIR = $PACKER_CACHE_DIR"
+
+# now run the Packer build
 mkdir -p ${flavour}
 (cd ${basebox}; packer build -only=${flavour}-iso template.json)
