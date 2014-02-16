0876810f80cd9d1029fe61ecc84f9d89ba169c69
Dieter Reuter <dieter.reuter@me.com>
2014-02-16 11:16:48 +0100

    Cleanup

--- build-basebox.sh
+++ build-basebox.sh
@@ -1,11 +1,7 @@
 #!/bin/bash 
-
 basebox=./template/dieter-win7x64sp1
 flavour=virtualbox
 
-
 echo "Building basebox: ${basebox} for ${flavour}"
-
 mkdir -p ${flavour}
 (cd ${basebox}; packer build -only=${flavour}-iso template.json)
-
